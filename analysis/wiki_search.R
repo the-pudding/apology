68543392c2fecae7b352c622f5f31ae8348afdf3
Arjun Kakkar <arjunkakkar@arjuns-mbp-2.home>
2019-10-07 13:09:36 -0400

    Updated wiki search function to return more refined apology candidates. Streamlined script.

--- analysis/wiki_search.R
+++ analysis/wiki_search.R
@@ -11,26 +11,35 @@ getResultsWiki <- function(x) {
   request <- paste0('https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=',
                     gsub('\\s|\\&|\\#', '%20', as.character(x)), 
                     '&prop=info&inprop=url&format=json')
-  
+
   results <- read_html(request) %>% html_node('p') %>% html_text() %>% fromJSON
-  if (length(results) < 3) return(NULL)
-  res_df <- bind_rows(unlist(results[[3]], recursive = FALSE)) %>%
+  if (length(results$query) == 0) return(NULL)
+  res_df <- bind_rows(unlist(results$query, recursive = FALSE)) %>%
     arrange(index) 
   
-  page_url <- res_df$fullurl[1]
   
-  page_lines <- read_html(page_url) %>% 
-    html_nodes('#bodyContent') %>% 
-    html_text %>% 
+  for (i in 1:nrow(res_df)) {
+    page_url <- res_df$fullurl[i]
+    
+    page_lines <- read_html(page_url) %>% 
+      html_nodes('#bodyContent') %>% 
+      html_text %>%
+      tolower()
+    
+    if (length(grep('youtube star|youtuber', page_lines)) != 0) break
+  }
+  
+  if (length(grep('youtube star|youtuber', page_lines)) == 0) return(NULL)
+  
+  page_lines <- page_lines %>% 
     strsplit(split = '\n') %>%
-    unlist() %>%
-    tolower()
+    unlist() 
   
   match_list <- page_lines %>% 
     grep('apology|controversy|controversial|apologized|controversies',
          ., value = TRUE)
   
-  return(list(res_df, page_url, match_list))
+  return(list(page_url, match_list))
 }
 
 # Function to search channel on Wikitubia and report instances of keywords on the found page
@@ -42,7 +51,7 @@ getResultsTube <- function(x) {
   url_node <- read_html(request) %>%
     html_node('li.result') 
   
-  if(is.na(url_node)) return(list(NULL, NULL))
+  if (is.na(url_node)) return(list(NULL, NULL))
   
   page_url <- html_node(url_node, 'a') %>% 
     html_attr('href')
@@ -68,17 +77,19 @@ getResultsTube <- function(x) {
 #tube_apology_data <- mclapply(current_list$name, getResultsTube, mc.cores = getOption("mc.cores", 14L))
 #save(tube_apology_data, file = 'data/wikitubia_apology_data.RData')
 
-load('wikitubia_apology_data.RData')
+#load('data/wikipedia_apology_data.RData')
+#load('data/wikitubia_apology_data.RData')
 
 # Data frame containing channels with apologies and url detected
-temp <- current_list[sapply(tube_apology_data, function(item) length(item[[2]]) > 0),] %>%
-  mutate(numfound = 
-           sapply(tube_apology_data, 
-                  function(item) length(item[[2]]))[sapply(tube_apology_data, 
-                                                           function(item) length(item[[2]]) > 0)],
-         urlfound = 
-           sapply(tube_apology_data, 
-                  function(item) item[[1]])[sapply(tube_apology_data, 
-                                                   function(item) length(item[[2]]) > 0)])
+apology_data <- wiki_apology_data
 
-tempurls <- data.frame(unlist(unique(temp$urlfound)))
+apology_indices <- sapply(apology_data, function(item) length(item[[2]]) > 0)
+
+temp <- current_list %>%
+  mutate(numfound = sapply(apology_data, function(item) length(item[[2]])),
+         urlfound = sapply(apology_data, function(item) item[[1]])) %>%
+  filter(apology_indices)
+
+unqiueurls <- data.frame(unlist(unique(temp$urlfound)))
+
+
