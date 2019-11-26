19de86301b3a868809eb77359ecddef05ff4e21e
Arjun Kakkar <arjunkakkar@arjuns-mbp-2.home>
2019-11-25 20:23:28 -0500

    added opacity change on hover and dynamic height scaling

--- web/src/js/graphic-beeswarm.js
+++ web/src/js/graphic-beeswarm.js
@@ -1,39 +1,50 @@
-import loadData from './load-data';
-import './pudding-chart/beeswarm'
+import loadData from "./load-data";
+import "./pudding-chart/beeswarm";
 
 const $section = d3.select('[data-js="beeswarm"');
 const $graphic = $section.select('[data-js="beeswarm__graphic"');
 const $figure = $graphic.selectAll('[data-js="graphic__figure"');
 
-const RADIUS = 30;
-const MARGIN = 50;
-const HEIGHT = 300;
-
-let width, scale, sim;
-
 const charts = [];
 
 function resize() {
-	//  todo chart resizes
-	charts.forEach(chart => {
-		chart.resize().render();
-	});
+  charts.forEach(chart => {
+    chart.resize().render();
+  });
 }
 
 function setupGraphics() {
-	const $f = d3.select(this);
-    const id = $f.attr('data-id');
-    
-	const file = `beeswarm--${id}.csv`;
-	loadData(file).then(data => {
-		const chart = $f.datum(data).puddingChartBeeswarm();
-		chart.resize().render();
-		charts.push(chart);
+  const $f = d3.select(this);
+  const id = $f.attr("data-id");
+
+  const file = `beeswarm--${id}.csv`;
+  loadData(file).then(data => {
+    const chart = $f.datum(data).puddingChartBeeswarm();
+    chart.resize().render();
+    chart
+      .getBees()
+      .nodes()
+      .map(setupHover);
+    charts.push(chart);
+  });
+}
+
+function setupHover(el) {
+  d3.select(el)
+    .on("mouseover", highlightEl)
+    .on("mouseout", () => {
+      d3.selectAll(".bee").transition().style("opacity", 1);
     });
 }
 
+function highlightEl() {
+  let $dataAttr = d3.select(this).attr("data-js");
+  d3.selectAll(".bee").transition().style("opacity", 0.3);
+  d3.selectAll(`[data-js=${$dataAttr}`).transition().style("opacity", 1);
+}
+
 function init() {
-    $figure.each(setupGraphics);
+  $figure.each(setupGraphics);
 }
 
 export default { init, resize };
