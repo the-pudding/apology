e6461bb0aa8a536159b7f48aaa9ae21849ef35d5
russellgoldenberg <russellgoldenberg@gmail.com>
2019-12-11 15:14:18 -0500

    nerd

--- web/src/js/graphic-beeswarm.js
+++ web/src/js/graphic-beeswarm.js
@@ -1,5 +1,5 @@
-import loadData from "./load-data";
-import "./pudding-chart/beeswarm";
+import loadData from './load-data';
+import './pudding-chart/beeswarm';
 
 const $section = d3.select('[data-js="beeswarm"]');
 const $graphic = $section.select('[data-js="beeswarm__graphic"]');
@@ -10,19 +10,19 @@ const charts = [];
 
 function slide(value) {
   const focusSlides = [
-    "GabrielZamora",
-    "JeffreeStar",
-    "JamesCharles",
-    "JaclynHill",
-    "LauraLee"
+    'GabrielZamora',
+    'JeffreeStar',
+    'JamesCharles',
+    'JaclynHill',
+    'LauraLee',
   ];
 
-  d3.select(".swiper").style("pointer-events", "none");
+  d3.select('.swiper').style('pointer-events', 'none');
   mouseOutHandler();
 
   if (focusSlides.includes(value)) {
-    d3.select(".swiper").style("pointer-events", "auto");
-    let $els = d3.selectAll(`[data-js="bee--${value}"`);
+    d3.select('.swiper').style('pointer-events', 'auto');
+    const $els = d3.selectAll(`[data-js="bee--${value}"`);
     highlightEl($els.node());
     $els.each(function(d) {
       hoverText(this, d);
@@ -33,7 +33,7 @@ function slide(value) {
 function resize() {
   const h = d3.select('[data-type="text"] .slide__text').node().offsetHeight;
   const sz = Math.floor(($section.node().offsetHeight - h) / $chart.size());
-  $chart.style("height", `${sz}px`);
+  $chart.style('height', `${sz}px`);
   charts.forEach(chart => {
     chart.resize().render();
   });
@@ -42,15 +42,15 @@ function resize() {
 function cleanData(data) {
   const clean = data.map(d => ({
     ...d,
-    beauty: d.beauty === "TRUE"
+    beauty: d.beauty === 'TRUE',
   }));
-  const filtered = clean.filter(d => d.value !== "NA");
+  const filtered = clean.filter(d => d.value !== 'NA');
   return filtered;
 }
 
 function setupGraphics() {
   const $f = d3.select(this);
-  const id = $f.attr("data-id");
+  const id = $f.attr('data-id');
 
   const file = `beeswarm--${id}.csv`;
   loadData(file)
@@ -68,8 +68,8 @@ function setupGraphics() {
 
 function setupHover(el) {
   d3.select(el)
-    .on("mouseover", mouseInHandler)
-    .on("mouseout", mouseOutHandler);
+    .on('mouseover', mouseInHandler)
+    .on('mouseout', mouseOutHandler);
 }
 
 function mouseInHandler(data) {
@@ -78,55 +78,54 @@ function mouseInHandler(data) {
 }
 
 function mouseOutHandler() {
-  d3.selectAll(".bee")
+  d3.selectAll('.bee')
     .transition()
     .duration(250)
     .ease(d3.easeCubicInOut)
-    .style("opacity", 1);
+    .style('opacity', 1);
   d3.selectAll('[data-js="beeswarm__hovertext"]')
     .transition()
     .duration(250)
     .ease(d3.easeCubicInOut)
-    .style("opacity", 0);
+    .style('opacity', 0);
 }
 
 function hoverText(elem, data) {
   const $fig = elem.parentElement.parentElement;
   const dims = elem.getBoundingClientRect();
   const $hoverBox = d3.select(
-    `[data-id=beeswarm__hovertext_${$fig.getAttribute("data-id")}`
+    `[data-id=beeswarm__hovertext_${$fig.getAttribute('data-id')}`
   );
   $hoverBox.select('[data-js="beeswarm__hovertext__title"]').html(data.name);
   $hoverBox.select('[data-js="beeswarm__hovertext__content"]').html(data.value);
-  let xoff =
+  const xoff =
     data.display < 0.2
       ? 0
       : data.display < 0.8
       ? $hoverBox.node().getBoundingClientRect().width / 2
       : $hoverBox.node().getBoundingClientRect().width;
   $hoverBox
-    .classed("is-beauty", data.beauty)
-    .style("left", `${dims.x + dims.width / 2 - xoff}px`)
-    .style("top", `${dims.y}px`)
+    .classed('is-beauty', data.beauty)
+    .style('left', `${dims.x + dims.width / 2 - xoff}px`)
+    .style('top', `${dims.y}px`)
     .transition()
     .duration(250)
     .ease(d3.easeCubicInOut)
-    .style("opacity", 1);
-  
+    .style('opacity', 1);
 }
 
 function highlightEl(elem) {
-  const $dataAttr = d3.select(elem).attr("data-js");
-  d3.selectAll(".bee")
+  const $dataAttr = d3.select(elem).attr('data-js');
+  d3.selectAll('.bee')
     .transition()
     .duration(250)
     .ease(d3.easeCubicInOut)
-    .style("opacity", 0.25);
+    .style('opacity', 0.25);
   d3.selectAll(`[data-js=${$dataAttr}]`)
     .transition()
     .duration(250)
     .ease(d3.easeCubicInOut)
-    .style("opacity", 1);
+    .style('opacity', 1);
 }
 
 function init() {
